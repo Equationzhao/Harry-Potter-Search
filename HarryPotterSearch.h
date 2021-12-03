6da3e4c6f646d15ac7467c1149cd71a503b32bfb
赵方程 <equationzhao@foxmail.com>
2021-12-03 12:44:45 +0800

    代码格式化

--- HarryPotterSearch.h
+++ HarryPotterSearch.h
@@ -1,8 +1,8 @@
 #pragma once
 #ifndef _HARRY_POTTER_SEARCH_
 #define _HARRY_POTTER_SEARCH_
-#include <string>
 #include <regex>
+#include <string>
 inline auto info() -> void;
 inline auto softwareInformation() -> void;
 auto initial() -> void;
@@ -11,57 +11,48 @@ auto gotoSearch() -> void;
 auto search() -> void;
 auto showTitle() -> void;
 auto showOutcome() -> void;
-auto checkNum(const char charNum[], int& n) -> bool;
-auto gotoRecord(const int&) -> void;
-auto isDigit(const std::string& str) -> bool;
+auto checkNum(const char charNum[], int &n) -> bool;
+auto gotoRecord(const int &) -> void;
+auto isDigit(const std::string &str) -> bool;
 auto strict = false;
 
-auto isDigit(const std::string& str) -> bool
-{
-	auto flag(true);
-	for (auto& i : str)
-	{
-		if (!isdigit(i))
-		{
-			flag = false;
-			break;
-		}
-	}
-	return flag;
+auto isDigit(const std::string &str) -> bool {
+    auto flag(true);
+    for (auto &i : str) {
+        if (!isdigit(i)) {
+            flag = false;
+            break;
+        }
+    }
+    return flag;
 }
 
-class isChapterClass
-{
+class isChapterClass {
 public:
-	auto operator()(const std::string& i)->bool
-	{
-		if (i.size() >= 25)
-		{
-			return false;
-		}
-		const std::regex pattern("chapter", std::regex::icase);
-		return std::regex_search(i.begin(), i.end(), pattern);
-	}
+    auto operator()(const std::string &i) -> bool {
+        if (i.size() >= 25) {
+            return false;
+        }
+        const std::regex pattern("chapter", std::regex::icase);
+        return std::regex_search(i.begin(), i.end(), pattern);
+    }
 };
 
-class isPageClass
-{
+class isPageClass {
 public:
-	auto operator()(const std::string& i) -> bool
-	{
-		return !i.empty() && i.size() < 4 && isdigit(i.at(0));
-	}
+    auto operator()(const std::string &i) -> bool {
+        return !i.empty() && i.size() < 4 && isdigit(i.at(0));
+    }
 };
 
-namespace option
-{
-	char exit[] = "exit";
-	char search[] = "search";
-	char Goto[] = "goto";
-	char help[] = "help";
-	char clear[] = "clear";
-	char information[] = "info";
-	char strictOn[] = "strictOn";
-	char strictOff[] = "strictOff";
-}
+namespace option {
+    char exit[] = "exit";
+    char search[] = "search";
+    char Goto[] = "goto";
+    char help[] = "help";
+    char clear[] = "clear";
+    char information[] = "info";
+    char strictOn[] = "strictOn";
+    char strictOff[] = "strictOff";
+}// namespace option
 #endif
--- searchMap.h
+++ searchMap.h
@@ -8,86 +8,74 @@
  * \brief 查找器
  * 用于查找page和chapter
  */
-class searchMap
-{
+class searchMap {
 private:
-	searchMap() : p_lastLine(0), c_lastLine(0) {};
-	std::vector<int> lpage_;
-	std::vector<int> page_;
-	std::vector<int> pchapter_;
-	std::vector<std::string> chapter_;
-	int p_lastLine;
-	int c_lastLine;
+    searchMap() : p_lastLine(0), c_lastLine(0){};
+    std::vector<int> lpage_;
+    std::vector<int> page_;
+    std::vector<int> pchapter_;
+    std::vector<std::string> chapter_;
+    int p_lastLine;
+    int c_lastLine;
+
 public:
-	static searchMap* mainSearcher;
+    static searchMap *mainSearcher;
 
-	~searchMap()
-	{
-		delete mainSearcher;
-	}
+    ~searchMap() {
+        delete mainSearcher;
+    }
 
-	auto setPage(const int& pLine, const int& page) -> void
-	{
-		lpage_.emplace_back(pLine);
-		page_.emplace_back(page);
-	}
+    auto setPage(const int &pLine, const int &page) -> void {
+        lpage_.emplace_back(pLine);
+        page_.emplace_back(page);
+    }
 
-	auto setChapter(const int& pLine, const std::string& chapter) -> void
-	{
-		pchapter_.emplace_back(pLine);
-		chapter_.emplace_back(chapter);
-	}
+    auto setChapter(const int &pLine, const std::string &chapter) -> void {
+        pchapter_.emplace_back(pLine);
+        chapter_.emplace_back(chapter);
+    }
 
-	auto searchPage(const int& line) -> int
-	{
-		auto p_size = lpage_.size();
-		for (auto i = p_lastLine; i < p_size; i++)
-		{
-			if (lpage_[i] > line)
-			{
-				p_lastLine = i;
-				return page_[i];
-			}
-		}
-		return 0;
-	}
+    auto searchPage(const int &line) -> int {
+        auto p_size = lpage_.size();
+        for (auto i = p_lastLine; i < p_size; i++) {
+            if (lpage_[i] > line) {
+                p_lastLine = i;
+                return page_[i];
+            }
+        }
+        return 0;
+    }
 
-	auto searchChapter(const int& line) -> std::string
-	{
-		auto c_size = pchapter_.size();
-		for (auto i = c_lastLine; i < c_size; i++)
-		{
-			if (pchapter_[i] > line)
-			{
-				c_lastLine = i;
-				return chapter_[i];
-			}
-		}
-		return chapter_.back();
-	}
+    auto searchChapter(const int &line) -> std::string {
+        auto c_size = pchapter_.size();
+        for (auto i = c_lastLine; i < c_size; i++) {
+            if (pchapter_[i] > line) {
+                c_lastLine = i;
+                return chapter_[i];
+            }
+        }
+        return chapter_.back();
+    }
 
-	auto operator=(const searchMap& rhs) -> searchMap&
-	{
-		lpage_ = rhs.lpage_;
-		chapter_ = rhs.chapter_;
-		pchapter_ = rhs.pchapter_;
-		page_ = rhs.page_;
-		return *this;
-	}
+    auto operator=(const searchMap &rhs) -> searchMap & {
+        lpage_ = rhs.lpage_;
+        chapter_ = rhs.chapter_;
+        pchapter_ = rhs.pchapter_;
+        page_ = rhs.page_;
+        return *this;
+    }
 
-	auto cpy(const searchMap& rhs)->void
-	{
-		lpage_ = rhs.lpage_;
-		chapter_ = rhs.chapter_;
-		pchapter_ = rhs.pchapter_;
-		page_ = rhs.page_;
-	}
+    auto cpy(const searchMap &rhs) -> void {
+        lpage_ = rhs.lpage_;
+        chapter_ = rhs.chapter_;
+        pchapter_ = rhs.pchapter_;
+        page_ = rhs.page_;
+    }
 
-	auto initial()->void
-	{
-		c_lastLine = 0;
-		p_lastLine = 0;
-	}
+    auto initial() -> void {
+        c_lastLine = 0;
+        p_lastLine = 0;
+    }
 };
 
 #endif
